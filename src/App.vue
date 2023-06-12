3479eb4658c1b80565b3429127a1bb15b9071a20
DanielaVenturi <danielaventuri14@gmail.com>
2023-06-12 10:54:48 -0300

    aa

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+ <div class="barra">
+  <div class="logo">Logo</div>
+  <div class="inicio"><button>Inicio</button></div>
+  <div class="quiz"><button>Quiz</button></div>
+  <div class="pontuacao"><button>Pontuaçao</button></div>
+ </div>
+
+ <div></div>
+</template>
+
+<style scoped>
+
+</style>
