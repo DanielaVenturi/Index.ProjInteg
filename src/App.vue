50db2c49cf41772ce2521ee4845b327f414cd116
DanielaVenturi <danielaventuri14@gmail.com>
2023-07-03 11:01:46 -0300

    ARRAY

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,25 @@
 </script>
 
 <template>
- <div class="barra">
-  <div class="logo">Logo</div>
-  <div class="inicio"><button>Inicio</button></div>
-  <div class="quiz"><button>Quiz</button></div>
-  <div class="pontuacao"><button>Pontuaçao</button></div>
- </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Quiz</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Início</a>
+        <a class="nav-link" href="#">Quiz</a>
+        <a class="nav-link" href="#">Pontuaçao</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
 
- <div></div>
+
+
 </template>
 
 <style scoped>
